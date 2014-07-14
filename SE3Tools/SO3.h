ec3e176f444822041c8cc3ed259bf0ca7f25bbf1
Hunter Gilbert <hunter.b.gilbert@gmail.com>
2014-07-14 16:23:03 -0500

    added licensing information for SE3 Tools, using GPL v3 License.

--- SE3Tools/SO3.h
+++ SE3Tools/SO3.h
@@ -1,3 +1,18 @@
+//This file is part of SE3 Tools.
+
+//SE3 Tools is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//SE3 Tools is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with SE3 Tools.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <Eigen/Dense>
 
 #if ( defined(WIN32) || defined( _WIN64 ) ) && defined( BUILD_SHARED_LIB )
